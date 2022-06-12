5b56fed04a307b90689bb1fa76387b241f2b84c5
Keith <claymorebusinessinquiry@gmail.com>
2022-06-12 00:56:19 -0400

    updated

--- src/main/java/com/microfocus/demo/usermanager/UsermanagerApplication.java
+++ src/main/java/com/microfocus/demo/usermanager/UsermanagerApplication.java
@@ -19,7 +19,7 @@ public class UsermanagerApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("https://microfocus-demo-web.vercel.app:443")); // TODO: Change to envvar
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://34.225.243.78:4200"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
