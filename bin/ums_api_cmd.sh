e1f6d689cca288116dd1214ab9737f67d95917a3
Keith <claymorebusinessinquiry@gmail.com>
2022-06-13 00:13:46 -0400

    updated binaries

---
+++ bin/ums_api_cmd.sh
@@ -0,0 +1,1 @@
+java -jar ./UMS_API.jar
--- src/main/java/com/microfocus/demo/usermanager/UsermanagerApplication.java
+++ src/main/java/com/microfocus/demo/usermanager/UsermanagerApplication.java
@@ -19,7 +19,7 @@ public class UsermanagerApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://34.225.243.78:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://34.225.243.78:4200")); // <= TODO: Replace with envvar
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
